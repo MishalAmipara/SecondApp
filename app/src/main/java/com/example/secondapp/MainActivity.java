0ce16b4cdfe9aa0be2fbb00d711660a1d1da157f
SahilDethaliya <sahildethaliya123@gmail.com>
2024-09-26 17:48:13 +0530

    first commit

--- app/src/main/java/com/example/secondapp/MainActivity.java
+++ app/src/main/java/com/example/secondapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.secondapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+<<<<<<< HEAD
 
 public class MainActivity extends AppCompatActivity {
 
+=======
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt1,txt2;
+>>>>>>> 7d7b4b7 (first commit)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+=======
+        txt1=findViewById(R.id.txt1);
+        txt2=findViewById(R.id.txt2);
+>>>>>>> 7d7b4b7 (first commit)
     }
 }
